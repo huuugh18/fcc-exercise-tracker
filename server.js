c28f8cc3e259e1f9704217a7053de37d8e4326d8
Hugh O'Neill <hugh.oneill.4@gmail.com>
2019-03-15 16:08:51 -0600

    add user to database success

--- server.js
+++ server.js
@@ -26,6 +26,58 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+const userSchema = new Schema ({
+    user_name: {type: String, required: true},
+    log: [{
+        description: {type:String},
+        duration: {type: Number},
+        date: {type: Date, default: Date.now}
+    }],
+});
+const User = mongoose.model('User', userSchema)
+// const exerciseSchema = new Schema({
+//     description: {type: String, required: true},
+//     duration: {type: Number, required:true},
+//     date: {type: String, required: true}
+// })
+// const Exercise = mongoose.model('Exercise', exerciseSchema)
+// 1) add a user on  - POST /api/exercise/new-user
+// return object with username and db id
+// check if username already in use
+app.post('/api/exercise/new-user', (req,res) => {
+    const userInput = req.body.username;
+    User.findOne({user_name:userInput}, (err, data) =>{
+        if(err){console.log(err)};
+        // if data null carry on with adding user
+        // if data not null - user iname in use 
+        if(data) { return res.json({error:`user name ${userInput} taken`}) };
+        const userDB = new User ({ user_name: userInput });
+        userDB.save( (err,data) => {
+            if(err){console.log('SAVE ERROR:',err)}
+            res.json({user_name:data.user_name,userId:data._id});
+        });
+    });
+});
+// 2) get array of all users - GET /api/exercise/users
+// return array of objects with username and _id for each user
+app.get('/api/exercise/users', (req,res) => {
+
+})
+// 3)add an exercise log to a user - POST /api/exercise/add
+// get user id from form, log to that user - check to make sure user exists
+// log = {description, duration, date(optional, if blank default to current date)}
+// return user object with exercise fields added
+
+// 4) get full ex log of any user - GET /api/exercise/log with param userId(_id)
+// return user object with added array log and count (total exercise count)
+
+// 5) can get part of ex log with params of from & to or limit
+// date format yyyy-mm-dd, limit = int
+
+// check if id for user valid
+
+
+// post logs of user
 
 // Not found middleware
 app.use((req, res, next) => {
